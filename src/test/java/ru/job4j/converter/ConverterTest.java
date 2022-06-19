53f9084aed747d1e5f1cf108d71876cffe27aeb4
sanya <batalovalexande@gmail.com>
2022-06-19 22:38:51 +0500

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,11 +15,12 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert200RblThen3dot3333Dlr() {
-        float in = 180;
-        float expected = 3;
-        float out = Converter.rubleToDollar(in);
+    public void rubleToEuro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
+
 }
