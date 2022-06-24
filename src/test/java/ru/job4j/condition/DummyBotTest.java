06c9742d57a88c8314e091b83077b070d66d2579
sanya <batalovalexande@gmail.com>
2022-06-24 23:04:14 +0500

    4.1.2. Глупый бот [#31323 #315370]

--- src/test/java/ru/job4j/condition/DummyBotTest.java
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -3,6 +3,7 @@ package ru.job4j.condition;
 import org.junit.Assert;
 import org.junit.Test;
 public class DummyBotTest {
+
     @Test
     public void whenGreetBot() {
         String in = "Привет, Бот.";
