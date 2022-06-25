504209302b48b3eff25d58417df3e7c7e2b03125
sanya <batalovalexande@gmail.com>
2022-06-25 21:13:58 +0500

    4.1. Операторы ветвлений [#3611 #315040]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class AlertDivByZero {
     public static void main(String[] args) {
-        possibleDiv(-1);
+        possibleDiv(4);
         possibleDiv(0);
     }
 
@@ -10,5 +10,9 @@ public class AlertDivByZero {
         if (number == 0) {
             System.out.println("This is negative number");
         }
+        if (number < 0) {
+            System.out.println("This is negative number");
+        }
+        }
     }
-}
+
