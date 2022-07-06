851e890f04a11045805815c001835c6d833c578e
sanya <batalovalexande@gmail.com>
2022-07-06 20:57:28 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/array/ArrayDefinition.java
+++ src/main/java/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
       float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+         names[0] = "Alexander Batalov";
+        System.out.println(names[0]);
+         names[1] = "Smirnov Alex";
+        System.out.println(names[1]);
+         names[2] = "Jessica Alba";
+        System.out.println(names[2]);
+         names[3] = "John Brown";
+        System.out.println(names[3]);
+
     }
 }
