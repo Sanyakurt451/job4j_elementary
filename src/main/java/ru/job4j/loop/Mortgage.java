84c1e3115648b734f87320ff4e1c28b4fc812eaf
sanya <batalovalexande@gmail.com>
2022-07-06 12:28:59 +0500

    5.7. Ипотека [#156315 #318945]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        while (year < amount) {
-        amount = (amount * percent) - salary;
-        salary = (amount * percent) - salary / amount;
-
-        year++;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
+            year++;
         }
         return year;
     }
